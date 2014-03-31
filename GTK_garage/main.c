fac0156856b243efcd08665e19fab690aa190ae8
thetacore <thetacore@gmail.com>
2014-03-31 14:37:32 +0300

    GTK

--- GTK_garage/main.c
+++ GTK_garage/main.c
@@ -1,49 +1,106 @@
-#include <stdlib.h>
 #include <gtk/gtk.h>
 
-static void helloWorld (GtkWidget *wid, GtkWidget *win)
+void show_info(GtkWidget* widget, gpointer window)
 {
-  GtkWidget *dialog = NULL;
-
-  dialog = gtk_message_dialog_new (GTK_WINDOW (win), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CLOSE, "Hello World!");
-  gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER);
-  gtk_dialog_run (GTK_DIALOG (dialog));
-  gtk_widget_destroy (dialog);
+    GtkWidget* dialog;
+    dialog = gtk_message_dialog_new(GTK_WINDOW(window), GTK_DIALOG_DESTROY_WITH_PARENT,
+                                    GTK_MESSAGE_INFO,
+                                    GTK_BUTTONS_OK,
+                                    "Download Completed", "title");
+                                    gtk_window_set_title(GTK_WINDOW(dialog), "Information");
+                                    gtk_dialog_run(GTK_DIALOG(dialog));
+                                    gtk_widget_destroy(dialog);
 }
 
-int main (int argc, char *argv[])
+
+void show_error(GtkWidget* widget, gpointer window)
 {
-  GtkWidget *button = NULL;
-  GtkWidget *win = NULL;
-  GtkWidget *vbox = NULL;
+    GtkWidget* dialog;
+    dialog = gtk_message_dialog_new(GTK_WINDOW(window),
+                                    GTK_DIALOG_DESTROY_WITH_PARENT,
+                                    GTK_MESSAGE_ERROR,
+                                    GTK_BUTTONS_OK,
+                                    "Error loading file");
+    gtk_window_set_title(GTK_WINDOW(dialog), "Error");
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+}
 
-  /* Initialize GTK+ */
-  g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, (GLogFunc) gtk_false, NULL);
-  gtk_init (&argc, &argv);
-  g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, g_log_default_handler, NULL);
+void show_question(GtkWidget* widget, gpointer window)
+{
+    GtkWidget* dialog;
+    dialog = gtk_message_dialog_new(GTK_WINDOW(window),
+                                    GTK_DIALOG_DESTROY_WITH_PARENT,
+                                    GTK_MESSAGE_QUESTION,
+                                    GTK_BUTTONS_YES_NO,
+                                    "Are you sure to quit?");
+    gtk_window_set_title(GTK_WINDOW(dialog), "Question");
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+}
 
-  /* Create the main window */
-  win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_container_set_border_width (GTK_CONTAINER (win), 8);
-  gtk_window_set_title (GTK_WINDOW (win), "Hello World");
-  gtk_window_set_position (GTK_WINDOW (win), GTK_WIN_POS_CENTER);
-  gtk_widget_realize (win);
-  g_signal_connect (win, "destroy", gtk_main_quit, NULL);
+void show_warning(GtkWidget* widget, gpointer window)
+{
+    GtkWidget* dialog;
+    dialog = gtk_message_dialog_new(GTK_WINDOW(window),
+                                    GTK_DIALOG_DESTROY_WITH_PARENT,
+                                    GTK_MESSAGE_WARNING,
+                                    GTK_BUTTONS_OK,
+                                    "Unallowed operation");
+    gtk_window_set_title(GTK_WINDOW(dialog), "Warning");
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
+}
 
-  /* Create a vertical box with buttons */
-  vbox = gtk_vbox_new (TRUE, 6);
-  gtk_container_add (GTK_CONTAINER (win), vbox);
+int main(int argc, char **argv)
+{
+    GtkWidget* window;
+    GtkWidget* table;
+    GtkWidget* info;
+    GtkWidget* warn;
+    GtkWidget* que;
+    GtkWidget* err;
 
-  button = gtk_button_new_from_stock (GTK_STOCK_DIALOG_INFO);
-  g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (helloWorld), (gpointer) win);
-  gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
+    gtk_init(&argc, &argv);
 
-  button = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
-  g_signal_connect (button, "clicked", gtk_main_quit, NULL);
-  gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(GTK_WINDOW(window), 220, 150);
+    gtk_window_set_title(GTK_WINDOW(window), "Message Dialog");
 
-  /* Enter the main loop */
-  gtk_widget_show_all (win);
-  gtk_main ();
-  return 0;
+    table = gtk_table_new(2, 2, TRUE);
+    gtk_table_set_row_spacings(GTK_TABLE(table), 2);
+    gtk_table_set_col_spacings(GTK_TABLE(table), 2);
+
+    info = gtk_button_new_with_label("Info");
+    warn = gtk_button_new_with_label("Warning");
+    que = gtk_button_new_with_label("Question");
+    err = gtk_button_new_with_label("Error");
+
+    gtk_table_attach(GTK_TABLE(table), info, 0, 1, 0, 1, GTK_FILL, GTK_FILL, 3, 3);
+    gtk_table_attach(GTK_TABLE(table), warn, 1, 2, 0, 1, GTK_FILL, GTK_FILL, 3, 3);
+    gtk_table_attach(GTK_TABLE(table), que, 0, 1, 1, 2, GTK_FILL, GTK_FILL, 3, 3);
+    gtk_table_attach(GTK_TABLE(table), err, 1, 2, 1, 2, GTK_FILL, GTK_FILL, 3, 3);
+
+    gtk_container_add(GTK_CONTAINER(window), table);
+    gtk_container_set_border_width(GTK_CONTAINER(window), 15);
+
+    g_signal_connect(G_OBJECT(info), "clicked",
+                     G_CALLBACK(show_info), (gpointer)window);
+
+    g_signal_connect(G_OBJECT(warn), "clicked",
+                     G_CALLBACK(show_warning), (gpointer)window);
+
+    g_signal_connect(G_OBJECT(que), "clicked",
+                     G_CALLBACK(show_question), (gpointer)window);
+
+    g_signal_connect(G_OBJECT(err), "clicked",
+                     G_CALLBACK(show_error), (gpointer)window);
+
+    g_signal_connect_swapped(G_OBJECT(window), "destroy",
+                             G_CALLBACK(gtk_main_quit), G_OBJECT(window));
+
+    gtk_widget_show_all(window);
+    gtk_main();
+    return 0;
 }
